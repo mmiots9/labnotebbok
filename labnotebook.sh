389b6b64a546c0771f08015e5cba4ae67150778d
Matteo Miotto pro <matteo.miotto9@gmail.com>
2023-10-03 17:59:04 +0200

    feat: add labnotebook --version function
    This adds the function labnotebook version that prints the current
    version of the package.

---
+++ labnotebook.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+labnotebook() {
+
+    ##------ CHECK INPUT ------##
+    if [[ $# -ne 1 ]]
+    then
+    echo "Usage: labnotebook --version"
+    return
+    fi
+
+    if [[ $1 != "--version" ]]
+    then
+    echo "Usage: labnotebook --version"
+    return
+    fi
+
+    ##------ PRINT VERSION ------##
+    echo "1.1.2-Linux"
+
+}
